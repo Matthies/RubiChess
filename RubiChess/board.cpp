c8be54303f11c73c771cda84ae2e36e8c0988b3e
Matthies <a.matthies@online.de>
2017-12-31 01:34:17 +0100

    Fixed bug that prevented clearing hash.

--- RubiChess/board.cpp
+++ RubiChess/board.cpp
@@ -2338,7 +2338,7 @@ void engine::communicate(string inputstring)
                 myUci->send("uciok\n", author);
                 break;
             case SETOPTION:
-                if (en.stopLevel != ENGINESTOPPED)
+                if (en.stopLevel < ENGINESTOPPED)
                 {
                     myUci->send("info string Changing option while searching is not supported.\n");
                     break;
