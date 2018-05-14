fe84cb5e7b94a031676a015d5de702f436724dd1
Andreas Matthies <a.matthies@online.de>
2018-05-14 13:48:39 +0200

    Added advanced lmr with dynamic reduction depending on depth and move number. 10+0.1: Score of RubiChess-Bitboard-lmr3 vs RubiChess-Bitboard-ms: 460 - 347 - 376  [0.548] 1183 Elo difference: 33.29 +/- 16.38 SPRT: llr 2.22, lbound -2.2, ubound 2.2 - H1 was accepted
    60+0.6:
    Score of RubiChess-Bitboard-lmr3 vs RubiChess-Bitboard-ms: 395 - 297 - 487  [0.542] 1179
    Elo difference: 28.95 +/- 15.20
    SPRT: llr 2.21, lbound -2.2, ubound 2.2 - H1 was accepted

--- RubiChess/RubiChess.h
+++ RubiChess/RubiChess.h
@@ -933,6 +933,7 @@ int rootsearch(int alpha, int beta, int depth);
 int alphabeta(int alpha, int beta, int depth, bool nullmoveallowed);
 int getQuiescence(int alpha, int beta, int depth);
 void searchguide();
+void searchinit();
 
 
 //
