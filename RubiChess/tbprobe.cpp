5e9bcb08ee09b5fd1292d4243339c38d4bff02ca
Matthies <a.matthies@online.de>
2019-02-10 09:02:08 +0100

    Added GPL3 file and text.

--- RubiChess/tbprobe.cpp
+++ RubiChess/tbprobe.cpp
@@ -1,8 +1,21 @@
 /*
-  Copyright (c) 2013-2015 Ronald de Man
-  This file may be redistributed and/or modified without restrictions.
+  RubiChess is a UCI chess playing engine by Andreas Matthies.
 
-  Adapted to RubiChess by Andreas Matthies
+  RubiChess is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Ethereal is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  This part of RubiChess is copyright (c) 2013-2015 Ronald de Man
+  and was adapted to RubiChess by Andreas Matthies
 */
 
 // The probing code currently expects a little-endian architecture (e.g. x86).
