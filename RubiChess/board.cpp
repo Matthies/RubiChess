2c5c27853e5f6b9036bf726e4804788a1ca22e15
Andreas Matthies <a.matthies@online.de>
2019-04-27 07:33:38 +0200

    Almost all tests pass [0,5]. So I will commit. Refinement may be possible later.

--- RubiChess/board.cpp
+++ RubiChess/board.cpp
@@ -190,7 +190,7 @@ void chessmovelist::sort(const unsigned int refutetarget)
     {
         if (refutetarget < BOARDSIZE && GETFROM(move[i].code) == refutetarget)
         {
-            //printf("refute\n");
+            // moves escaping from last null move refute target better than moves with negative history 
             move[i].value = max(0, move[i].value);
         }
         for (int j = i + 1; j < length; j++)
