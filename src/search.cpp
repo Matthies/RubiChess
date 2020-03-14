dc48106e1f097f8d1133700cd69263a99d17bde6
Andreas Matthies <a.matthies@online.de>
2020-03-14 21:39:58 +0100

    Use ply to index the movenumber stack. This should fix rare crashes visible at OpenBench.

--- src/search.cpp
+++ src/search.cpp
@@ -605,7 +605,7 @@ int chessposition::alphabeta(int alpha, int beta, int depth)
             reduction -= PVNode;
 
             // adjust reduction with opponents move number
-            reduction -= (LegalMoves[mstop] > 15);
+            reduction -= (LegalMoves[ply] > 15);
 
             STATISTICSINC(red_pi[positionImproved]);
             STATISTICSADD(red_lmr[positionImproved], reductiontable[positionImproved][depth][min(63, legalMoves + 1)]);
@@ -649,7 +649,7 @@ int chessposition::alphabeta(int alpha, int beta, int depth)
 
         STATISTICSINC(moves_played[(bool)ISTACTICAL(m->code)]);
 
-        LegalMoves[mstop] = ms.legalmovenum;
+        LegalMoves[ply] = ms.legalmovenum;
 
         if (reduction)
         {
