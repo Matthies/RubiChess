149b90b520ae1c05736bcea4affc295520005f17
Matthies <a.matthies@online.de>
2020-12-28 10:04:28 +0100

    Largepage (#260)
    * Implement use of large pages for Windows.
    
    * Handle error in memory allocation.
    
    * Fix memory free. Disable UseLargePages at first failure.
    
    * .

--- src/RubiChess.h
+++ src/RubiChess.h
@@ -603,6 +603,13 @@ string IndexToAlgebraic(int i);
 string AlgebraicFromShort(string s, chessposition *pos);
 void BitboardDraw(U64 b);
 U64 getTime();
+#ifdef _WIN32
+void* my_large_malloc(size_t s);
+void my_large_free(void *m);
+#else
+#define my_large_malloc(x) allocalign64(x)
+#define my_large_free(m) freealigned64(m)
+#endif
 #ifdef STACKDEBUG
 void GetStackWalk(chessposition *pos, const char* message, const char* _File, int Line, int num, ...);
 #endif
