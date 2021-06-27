83caf03ac9060589c48128af43f49026878f4924
Matthies <a.matthies@online.de>
2021-06-27 23:12:55 +0200

    Only recapture < -5.

--- src/search.cpp
+++ src/search.cpp
@@ -306,7 +306,7 @@ int chessposition::getQuiescence(int alpha, int beta, int depth)
 
     MoveSelector* ms = &moveSelector[ply];
     memset(ms, 0, sizeof(MoveSelector));
-    ms->SetPreferredMoves(this, depth < -4);
+    ms->SetPreferredMoves(this, depth < -5);
     STATISTICSINC(qs_loop_n);
 
     uint32_t bestcode = 0;
