0f9d700b9f28b0fa95733e33d3fe1253f64c530d
Matthies <a.matthies@online.de>
2022-01-15 09:23:53 +0100

    Fix. Write mate scores to the log.

--- src/search.cpp
+++ src/search.cpp
@@ -1270,7 +1270,7 @@ static void uciScore(searchthread *thr, int inWindow, U64 nowtime, int score, in
     else
     {
         int matein = (score > 0 ? (SCOREWHITEWINS - score + 1) / 2 : (SCOREBLACKWINS - score) / 2);
-        cout << "info depth " + to_string(thr->depth) + " seldepth " + to_string(pos->seldepth) + " multipv " + to_string(mpvIndex + 1) + " time " + to_string(msRun)
+        guiCom << "info depth " + to_string(thr->depth) + " seldepth " + to_string(pos->seldepth) + " multipv " + to_string(mpvIndex + 1) + " time " + to_string(msRun)
             + " score mate " + to_string(matein) + " " + boundscore[inWindow] + "nodes " + to_string(nodes) + " nps " + to_string(nps) + " tbhits " + to_string(en.tbhits)
             + " hashfull " + to_string(tp.getUsedinPermill()) + " pv " + pvstring + "\n";
     }
