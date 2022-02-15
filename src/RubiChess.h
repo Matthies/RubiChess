6e5b81ff2c6d37a5519b7a378e52f2e58bdd79a6
Matthies <a.matthies@online.de>
2022-02-15 12:53:23 +0100

    New network nn-d585174582-20220214.nnue (#338)

--- src/RubiChess.h
+++ src/RubiChess.h
@@ -22,7 +22,7 @@
 
 // Disable this to compile without NNUE evaluation
 #define NNUE
-#define NNUEDEFAULT nn-7e8d2f1670-20220202.nnue
+#define NNUEDEFAULT nn-d585174582-20220214.nnue
 
 // Enable to get statistical values about various search features
 //#define STATISTICS
