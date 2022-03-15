bdd064b849ef9ae4c85b1646f656c5d0cf1b4aaa
Matthies <a.matthies@online.de>
2022-03-15 13:19:10 +0100

    Movestogo (#344)
    * Avoid using too much time in movestogo mode.
    
    * Last patch was good at very shot TC 40/20 (avoids running out of time near move 40) but worse at 40/60.
    This one tries to relax stop immediate if many moves are left but avoid go run out of time when few moves are left.
    
    * Last test was better. Relax endtime2 even more when many moves are left.
    
    * 40/60.0 test still (and even more) bad. Try less relaxation.
    
    * Still not good at 40/60. Restrict endtime1 a little more.
    
    * tm sucks!. Last(?) try.
    
    * Back to the best working version.

--- src/search.cpp
+++ src/search.cpp
@@ -1540,12 +1540,12 @@ void resetEndTime(U64 startTime, int constantRootMoves, bool complete)
         // f2: stop immediately at 1.9...3.1 x average movetime
         // movevariation: many moves to go decrease f1 (stop soon)
         int movevariation = min(32, en.movestogo) * 3 / 32;
-        int f1 = max(10 - movevariation, 22 - movevariation - constance);
+        int f1 = max(9 - movevariation, 21 - movevariation - constance);
         int f2 = max(19, 31 - constance);
         int timeforallmoves = timetouse + en.movestogo * timeinc;
         if (complete)
             en.endtime1 = startTime + timeforallmoves * en.frequency * f1 / (en.movestogo + 1) / 10000;
-        en.endtime2 = startTime + min(max(0, timetouse - overhead), f2 * timeforallmoves / (en.movestogo + 1) / 10) * en.frequency / 1000;
+        en.endtime2 = startTime + min(max(0, timetouse - overhead), f2 * timeforallmoves / (en.movestogo + 1) / (19 -  4 * movevariation)) * en.frequency / 1000;
     }
     else if (timetouse) {
         if (timeinc)
