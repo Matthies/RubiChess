b60df46e430b3c2c219be1a720b73817d25b0467
Matthies <a.matthies@online.de>
2022-04-15 14:31:15 +0200

    Some work on the MSVC Makefile

--- src/release.cmd
+++ src/release.cmd
@@ -7,10 +7,22 @@
 
 set SDE=
 :: Some defaults for the SDE on my computer
-if exist C:\bin\SDE\sde.exe set SDE=C:\bin\SDE\sde.exe
+if exist C:\bin\SDE\sde.exe (
+  set SDE=C:\bin\SDE\sde.exe
+  echo Found SDE at C:\bin\SDE\sde.exe
+)
 
-set vcvarscmd=
-if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" set vcvarscmd=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat
+set vcvarscmd=%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat
+if exist "%vcvarscmd%" goto build
+
+set vcvarscmd=%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat
+if exist "%vcvarscmd%" goto build
+
+echo No supported MSVC build tools found. Exit!
+goto end
+
+:build
+echo Found MSVC Build tools %vcvarscmd%
 
 call "%vcvarscmd%" x64 -vcvars_ver=14.16
 nmake -f Makefile.clang release
@@ -18,4 +30,6 @@ nmake -f Makefile.clang release
 call "%vcvarscmd%" x64_arm64 -vcvars_ver=14.16
 nmake -f Makefile.clang release
 
+:end
 pause
+
