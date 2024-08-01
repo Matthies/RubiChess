55d22849a20d59d20e812d254d2a94796b144de5
Matthies <a.matthies@gmx.net>
2024-08-01 11:33:34 +0200

    Dynamic detection of LLVM folder for Windows Makefile. Bench: 6203151

--- src/release.cmd
+++ src/release.cmd
@@ -12,6 +12,26 @@ if exist C:\bin\SDE\sde.exe (
   echo Found SDE at C:\bin\SDE\sde.exe
 )
 
+if not exist "%ProgramFiles%\LLVM%clangver%" (
+  :: Clang not installed in generic folder; search for latest version
+  for /L %%v IN (30,-1,9) do (
+    if exist "%ProgramFiles%\LLVM%%v" (
+      set clangver=%%v
+      goto foundllvm
+    )
+  ) 
+)
+
+:foundllvm
+if exist "%ProgramFiles%\LLVM%clangver%" (
+  echo Found LLVM at "%ProgramFiles%\LLVM%clangver%"
+) else (
+  echo Cannot find LLVM at %ProgramFiles%\LLVM*. Exit!
+  goto end
+)
+
+
+
 set vcvarscmdicx="%ProgramFiles(x86)%\Intel\oneAPI\setvars.bat"
 set vcvarscmd22="%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat"
 set vcvarscmd19="%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat"
@@ -21,17 +41,19 @@ set vsver=
 if exist %vcvarscmd22% (
   set vcvarscmd=%vcvarscmd22%
   set vsver=vs2022
-  goto build
+  echo Found Visual Studio 2022 Tools
+  goto buildx64
 )
 if exist %vcvarscmd19% (
   set vcvarscmd=%vcvarscmd19%
   set vsver=vs2019
-  goto build
+  echo Found Visual Studio 2019 Tools
+  goto buildx64
 )
 echo No supported MSVC build tools found. Exit!
 goto end
 
-:build
+:buildx64
 ::
 :: x64-64 buils
 ::
@@ -45,6 +67,7 @@ if exist %vcvarscmdicx% (
   nmake -c -f Makefile.clang release COMP=clang
 )
 
+:buildarm
 ::
 :: ARM64 build
 ::
